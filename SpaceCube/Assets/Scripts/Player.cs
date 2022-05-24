6421b1c0e209f05bc8c7ff880d48e89fe4bb0511
slapota <arnost.spalensky@gmail.com>
2022-05-24 22:04:23 +0200

    ČIČIN

---
+++ SpaceCube/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController player;
+    float rotation;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+
+        rotation += Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        player.transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+        /*float z = Input.GetAxis("Vertical");
+        direction = transform.forward * z;
+        player.Move(direction * Time.deltaTime * speed);
+        direction = Vector3.zero;*/
+    }
+}
