e66d0143f2e0f7654af51b965bf19deea92f4538
slapota <arnost.spalensky@gmail.com>
2022-05-26 22:05:27 +0200

    čičin

---
+++ SpaceCube/Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    public GameObject planet;
+
+    public void RollForward()
+    {
+        Rotate(Vector3.forward);
+    }
+    public void RollBack()
+    {
+        Rotate(Vector3.back);
+    }
+    public void RollLeft()
+    {
+        Rotate(Vector3.left);
+    }
+    public void RollRight()
+    {
+        Rotate(Vector3.right);
+    }
+    void Rotate(Vector3 direction)
+    {
+        planet.transform.Rotate(direction * 90, Space.World);
+    }
+}
