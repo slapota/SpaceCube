e2856464ad0117cd072811cce35a7d9eae6e1d61
slapota <arnost.spalensky@gmail.com>
2022-06-12 18:44:04 +0200

    kočka

---
+++ SpaceCube/Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Image> items = new List<Image>();
+    public List<Text> amounts = new List<Text>();
+    [SerializeField] GameObject backpack;
+
+    void Start()
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            Image instance = Instantiate(items[i]);
+            instance.transform.position = new Vector2(i * 80 + 60, -60);
+            instance.transform.SetParent(backpack.transform, false);
+            amounts[i].text = instance.GetComponent<Item>().amount.ToString();
+        }
+    }
+}
